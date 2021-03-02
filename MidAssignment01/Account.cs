7e04a102a3ac007f6e2ffb74ea4437dd699086c2
Salma Jahan Sahara <saharasayed5@gmail.com>
2021-03-02 11:41:50 +0600

    Add project files.

---
+++ MidAssignment01/Account.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Account
+    {
+        private int accountNumber;
+        private string accountName;
+        private double balance;
+        private Address address;
+
+        //declaring parameterized constructor
+        public Account(int accountNumber, string accountName, double balance, Address address)
+        {
+            this.accountNumber = accountNumber;
+            this.accountName = accountName;
+            this.balance = balance; // storing a value
+            this.address = address; //storing an obj ref
+        }
+        public int AccountNumber
+        {
+            set { this.accountNumber = value; }
+            get { return this.accountNumber; }
+        }
+        public string AccountName
+        {
+            get { return this.accountName; }
+            set { this.accountName = value; }
+        }
+        public double Balance
+        {
+            set { this.balance = value; }
+            get { return this.balance;  }
+        }
+        public Address Address
+        {
+            set { this.address = value; }
+            get { return this.address; }
+        }
+        public void Withdraw(double amount)
+        {
+            this.balance = this.balance - amount;
+        }
+        public void Deposit(double amount)
+        {
+            this.balance = this.balance + amount;
+        }
+        public void Transfer(double amount, Account receiver)
+        {
+            this.balance = this.balance - amount;
+            a.balance = a.balance + amount;
+            //receiver = balance + amount;
+        }
+
+
+        public void ShowAccountInformation()
+        {
+            //Console.WriteLine("Account Number: " + this.accountNumber + "\nAccount Name: " + this.accountName + "\nBalance: " + this.balance);
+            Console.WriteLine("Account No:{0}\nAccount Name:{1}\nBalance:{2}", this.accountNumber, this.accountName, this.balance);
+            this.address.GetAddress();
+        }
+
+    }
+}
---
+++ MidAssignment01/Bank.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Bank
+    {
+        private string bankName;
+        private Account[] accounts;
+
+        public Bank(string name, int size)
+        {
+            this.bankName = name;
+            accounts = new Account[size];
+        }
+
+        public string Name
+        {
+            set { this.Name = value; }
+            get { return this.Name; }
+        }
+        public Account[] Accounts //returning the array
+        {
+            set { this.accounts = value; }
+            get { return this.accounts; }
+        }
+        public void PrintAccountDetails()
+        {
+            for(int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i] == null)
+                {
+                    continue;
+                }
+                accounts[i].ShowAccountInformation();
+            }
+        }
+        public void AddAccount(Account account)
+        {
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i] == null)
+                {
+                    accounts[i] = account;
+                    break;
+                }
+            }
+        }
+        public void RemoveAccount(int accountNumber)
+        {
+            int flag =0;
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i] == null)
+                {
+                    continue;
+                }
+                else if(accounts[i].AccountNumber == accountNumber)
+                {
+                    accounts[i].ShowAccountInformation();
+                    flag = 0;
+                    break;
+                }
+                else 
+                {
+                    flag = 1;
+                }
+
+                //accounts[i].ShowAccountInformation();
+            }
+            if (flag == 1)
+                Console.WriteLine("Account Not Found");
+        }
+    }
+}
